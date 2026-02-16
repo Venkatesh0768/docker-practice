538afee636f685a5ded6b88e3f19838a3198bfef
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-02-16 20:32:52 +0530

    Resolve the url mapping

--- frontend/src/API/api.jsx
+++ frontend/src/API/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `http://${window.location.hostname}:8081`;
+const BASE_URL = `http://${window.location.hostname}:8081/api/v1/`;
 
 const api = axios.create({
   baseURL: BASE_URL,
