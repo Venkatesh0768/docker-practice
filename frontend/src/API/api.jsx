c696dbce2043f873d8c157d982f1fa93ebebdccf
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2026-02-16 20:30:22 +0530

    Resolve the url mapping

--- frontend/src/API/api.jsx
+++ frontend/src/API/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
+const BASE_URL = `http://${window.location.hostname}:8081`;
 
 const api = axios.create({
   baseURL: BASE_URL,
